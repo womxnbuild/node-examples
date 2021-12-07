2d82d1cd235b1bda181308d7e4c1b6caf7955dd6
womxnbuilder <77127555+womxnbuild@users.noreply.github.com>
2021-12-07 15:39:18 -0500

    add rectangle module

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect = {
+    area: (x,y) => (x*y),
+    perimeter: (x,y) => (2*(x+y))
+}
+
+function solveRectangle(l, b){
+    console.log('solving for rectangle with length = %d and breadth = %d', l, b);
+    if (l <= 0 || b <= 0 ){
+        console.log('   [x] invalid dimensions for rectangle with length = %d and breadth = %d', l, b);
+    } else {
+        console.log('   [y] area of a rectangle is %d', rect.area(l,b));
+        console.log('   [y] perimeter of a rectangle is %d', rect.perimeter(l,b));
+    }
+}
+
+solveRectangle(2,0);
+solveRectangle(0,-4);
+solveRectangle(2,3);
