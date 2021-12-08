4c2d1e9a0bacc0518dfb73819bd53c0074854b52
womxnbuilder <77127555+womxnbuild@users.noreply.github.com>
2021-12-07 20:39:51 -0500

    use callbacks and remove {}

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const rectangle = require('./rectangle');
 var rect = require('./rectangle');
 
 function solveRectangle(l, b){
     console.log('solving for rectangle with length = %d and breadth = %d', l, b);
-    rectCallback = (err, rectCallback) => {
-        if (err){
-            console.log('ERROR: ', err.message);
+
+    rect(l, b , (err, rectCallback) => {
+        if (err) {
+            console.debug('ERROR: ', err.message);
         } else {
             console.log('   [y] area of a rectangle is %d', rect.area());
             console.log('   [y] perimeter of a rectangle is %d', rect.perimeter());
         }
-    }
-    rect(l, b , rectCallback);
+    });
+    console.log('** This concludes the end of operations\n');
 }
 
 solveRectangle(2,0);
--- rectangle.js
+++ rectangle.js
@@ -1,16 +1,17 @@
 module.exports = (x, y, callback) => {
-    if (l <= 0 || b <= 0 ){
-        setTimeout(() => {
-            callback(new Error('   [x] invalid dimensions for rectangle with length = %d and breadth = %d', x, y), null)
-        , 2000});
+    if (x <= 0 || y <= 0 ){
+        setTimeout(() =>
+            callback(new Error("   [x] invalid dimensions for rectangle with length = " +
+             x + " and breadth = " + y), null)
+        , 2000);
     } else {
-        setTimeout(() => {
+        setTimeout(() =>
             null, 
             {
                 permimeter:(x,y) => (2*(x+y)),
                 area: (x,y) => (x*y)
             }
-            , 2000});
+            , 2000);
     }
 }
 
